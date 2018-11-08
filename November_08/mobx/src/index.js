2359ea58ce81b29325d278caea62ecdef4101a42
andrewkiri <andrewkirichok@gmail.com>
2018-11-08 18:37:39 +0100

    mobx-boilerplate

--- November_08/mobx/src/index.js
+++ November_08/mobx/src/index.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app/App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'mobx-react';
+import React from "react";
+import { render } from "react-dom";
+import DevTools from "mobx-react-devtools";
 
-import stores from './stores';
+import TodoList from "./components/TodoList";
+import TodoListModel from "./models/TodoListModel";
+import TodoModel from "./models/TodoModel";
 
-ReactDOM.render(
-  <Provider {...stores}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+const store = new TodoListModel();
+
+render(
+  <div>
+    <DevTools />
+    <TodoList store={store} />
+  </div>,
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+store.addTodo("Get Coffee");
+store.addTodo("Write simpler code");
+store.todos[0].finished = true;
+
+setTimeout(() => {
+  store.addTodo("Get a cookie as well");
+}, 2000);
+
+// playing around in the console
+window.store = store;
--- November_08/mobx/src/stores/index.js
+++
@@ -1,9 +0,0 @@
-import CategoryStore from './CategoryStore'
-import MainStore from './MainStore'
-import CartStore from './CartStore'
-
-export default {
-  CategoryStore: new CategoryStore(),
-  MainStore: new MainStore(),
-  CartStore: new CartStore()
-}
