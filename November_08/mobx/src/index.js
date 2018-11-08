c0cebf5ae2152a5eea3959970a7632a1983db9c8
andrewkiri <andrewkirichok@gmail.com>
2018-11-08 18:51:40 +0100

    newer boilerplate

--- November_08/mobx/src/index.js
+++ November_08/mobx/src/index.js
@@ -1,28 +1,10 @@
-import React from "react";
-import { render } from "react-dom";
-import DevTools from "mobx-react-devtools";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import TodoList from "./components/TodoList";
-import TodoListModel from "./models/TodoListModel";
-import TodoModel from "./models/TodoModel";
+import App from './app'
 
-const store = new TodoListModel();
+const entry = document.getElementById('root')
 
-render(
-  <div>
-    <DevTools />
-    <TodoList store={store} />
-  </div>,
-  document.getElementById("root")
-);
-
-store.addTodo("Get Coffee");
-store.addTodo("Write simpler code");
-store.todos[0].finished = true;
-
-setTimeout(() => {
-  store.addTodo("Get a cookie as well");
-}, 2000);
-
-// playing around in the console
-window.store = store;
+ReactDOM.render(
+  <App/>, entry
+)
